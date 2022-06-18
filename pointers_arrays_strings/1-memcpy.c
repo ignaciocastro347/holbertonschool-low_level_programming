b3945a0a41ff5a0f79192d6ba71e5c08cd028ad5
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-17 22:19:52 -0700

    copies memory area.

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - fills memory with a constant byte.
+ * @dest: destiny string
+ * @src: constant byte to fill
+ * @n: times to fill s with b value
+ * Return: pointer to updated string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
