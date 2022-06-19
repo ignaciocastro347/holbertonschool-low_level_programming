f541402f3690c3dc23ae41d248bb94d9ced0e63d
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-19 15:05:08 -0700

    prints the chessboard.

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,10 +6,10 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j = 0;
-	while (i < 8)
+	int col = 0, row = 0;
+	while (col < 8)
 	{
-		while (j < 8)
+		while (row < 8)
 		{
 			printf("%d", a[row][col]);
 			row++;
