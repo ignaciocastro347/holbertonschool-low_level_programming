e2b592785f571ee2921c19e5ab242047be713ea6
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-19 15:12:18 -0700

    prints the chessboard.

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_chessboard - prints the chessboard
  * @a: two dimension array
@@ -9,12 +8,13 @@ void print_chessboard(char (*a)[8])
 	int col = 0, row = 0;
 	while (col < 8)
 	{
+		row = 0;
 		while (row < 8)
 		{
-			printf("%d", a[col][row]);
+			_putchar(a[col][row]);
 			row++;
 		}
-		printf("\n");
+		_putchar('\n');
 		col++;
 	}
 }
