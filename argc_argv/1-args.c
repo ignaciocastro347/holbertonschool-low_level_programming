747ba0eea26f347de107caf3a0c0d9728b6470b8
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-21 07:33:19 -0700

    prints the number of arguments passed into it.

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print this program name
+ * @argc: number of arguments
+ * @argv: vector with arguments
+ * Return: 0 in case everything is ok
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
