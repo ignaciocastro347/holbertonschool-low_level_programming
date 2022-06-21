2d15dd588a7068722fce01bc2f04fb0be215a428
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-21 07:23:53 -0700

    prints its name, followed by a new line.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print this program name
+ * @argc: number of arguments
+ * @argv: vector with arguments
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+Here are some exercises related to agrc and argv
