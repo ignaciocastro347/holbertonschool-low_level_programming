f199f58033cb52c6957579024c645ed3f8f92b10
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-21 08:12:27 -0700

    prints all arguments it receives.

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print every program argument
+ * @argc: number of arguments
+ * @argv: vector with arguments
+ * Return: 0 in case everything is ok
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+	while (argv[i])
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
