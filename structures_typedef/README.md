64799bfdba9c3fde8c70bf80ea55657e4da55344
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-28 10:44:33 -0700

    Define a new type struct dog

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+Here are some exercises about structures_typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct my_struct - a dog structure 
+ * @name: name as string
+ * @age: age as float
+ * @owner: owner as string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
