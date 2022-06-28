6f9dba2dafb83f22f5d06f85835a511bad2aecb9
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-28 11:56:59 -0700

    prints a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print each attribute of a dog
+ * @d: pointer to dog structure
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "nil");
+		printf("Age: %d\n", d->age || "nil");
+		printf("Owner: %s\n", d->owner ? d->owner : "nil");
+	}
+}
