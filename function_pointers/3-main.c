e0ff805c9ab89087be1299baf68a672abead8ec7
Ignacio Castro <ignaciocastro347@gmail.com>
2022-06-30 07:13:22 -0700

    program that performs simple operations.

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	if (argv[2][1])
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 	op_func = get_op_func(argv[2][0]);
 	printf("%d\n", op_func(atoi(argv[1]), atoi(argv[3])));
