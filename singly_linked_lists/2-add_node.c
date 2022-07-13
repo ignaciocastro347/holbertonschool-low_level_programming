e71284f84f3122b2d532fc9ec8579a81c6d6e237
Ignacio Castro <ignaciocastro347@gmail.com>
2022-07-13 08:48:01 -0700

    adds a new node at the beginning of a list_t list.[

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_node - add a node at the beginning of a list
+ * @head: destiny list
+ * @str: string value
+ * Return: a pointer to the new node of the list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	if (!str)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	/*head = new;*/
+	return (new);
+}
