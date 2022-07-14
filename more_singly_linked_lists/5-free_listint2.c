d2af3a2a3ad10251fc71ef1461ce7d43d9d2ecc3
Ignacio Castro <ignaciocastro347@gmail.com>
2022-07-14 14:35:07 -0700

    frees a listint_t list

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 /**
- * free_listint - free a linked list
+ * free_listint2 - free a linked list
  * @head: linked list
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *aux;
-
-	while (*head)
-	{
-		aux = (*head)->next;
-		free(*head);
-		*head = aux;
-	}
+	if (*head)
+		while (*head)
+		{
+			aux = (*head)->next;
+			free(*head);
+			*head = aux;
+		}
 }
