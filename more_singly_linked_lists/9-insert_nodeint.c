3637365d68c712ab4f3348f89761a89a2885ae2e
Ignacio Castro <ignaciocastro347@gmail.com>
2022-07-15 11:42:15 -0700

    inserts a new node at a given position

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include "2-add_nodeint.c"
 /**
  * insert_nodeint_at_index - inserta node with n value at index
  * @head: linked list
@@ -8,43 +9,26 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *aux;
+	listint_t *new, *aux = *head;
 	unsigned int i = 0;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (idx == 0)
+		return (add_nodeint(head, n));
+	if (!aux)
 		return (NULL);
-	if (!head)
-		return (NULL);
-	aux = *head;
 	while (aux)
 	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
+			if (!aux->next)
+				return (add_nodeint_end(head, n));
+			new = malloc(sizeof(listint_t));
+			if (!new)
+				return (NULL);
 			new->n = n;
-			if (i == 0)
-			{
-				new->next = *head;
-				*head = new;
-			}
-			else if (!aux->next)
-			{
-				new->n = n;
-				new->next = NULL;
-				aux->next = new;
-			}
-			else
-			{
-				new->next = aux->next;
-				aux->next = new;
-			}
-			return (new);
-		}
-		else if (!aux->next && i + 1 == idx)
-		{
-			new->n = n;
-			new->next = NULL;
+			new->next = aux->next;
 			aux->next = new;
+			return (new);
 		}
 		aux = aux->next;
 		i++;
