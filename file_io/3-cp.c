2a97145c843f2e86077d3096b96747d6e051f979
Ignacio Castro <ignaciocastro347@gmail.com>
2022-07-20 14:57:17 -0700

    copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,11 +16,6 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	finput = open(av[1], O_RDONLY);
-	if (finput == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-	        exit(98);
-	}
 	foutput = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((len = read (finput, buff, sizeof(buff))) < 0)
 	{
@@ -37,7 +32,7 @@ int main(int ac, char **av)
 	}
 	if (close(foutput) == -1 || close(finput) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", foutput);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", -1);
 		exit(100);
 	}
 	return (0);
