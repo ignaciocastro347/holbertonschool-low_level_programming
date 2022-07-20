5ddfb102eb64e94b52a204716f8d8f4894f156e5
Ignacio Castro <ignaciocastro347@gmail.com>
2022-07-20 14:12:43 -0700

    copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,7 +3,7 @@
  * main - copy content to a file and paste to another file
  * @filename: name of file to create
  * @text_content: content of file to write
- * Return: 1 on success, -1 on failure
+ * Return: 0 on success, -1 on failure
  */
 int main(int ac, char **av)
 {
@@ -12,35 +12,43 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(fileno(stderr), "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	finput = open(av[1], O_RDONLY);
 	if (finput == -1)
 	{
-		dprintf(fileno(stderr), "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 	        exit(98);
 	}
 	foutput = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (foutput == -1)
 	{
-		dprintf(fileno(stderr), "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 	        exit(99);
 	}
-	while ((len = read (finput, buff, sizeof(buff))) > 0)
+	while (0 == 0)
+	{
+		len = read (finput, buff, sizeof(buff));
+		if (len == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
 		if (write (foutput, buff, len) != len)
 		{
 			len = -1;
 			break;
 		}
+	}
 	if (close(finput) == -1)
 	{
-		dprintf(fileno(stderr), "Error: Can't close fd %i\n", finput);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", finput);
 		exit(100);
 	}
 	if (close(foutput) == -1)
 	{
-		dprintf(fileno(stderr), "Error: Can't close fd %i\n", foutput);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", foutput);
 		exit(100);
 	}
 	return (0);
