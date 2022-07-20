cce297a14144a0dee8e17694f2101853e4a890f7
Ignacio Castro <ignaciocastro347@gmail.com>
2022-07-20 14:26:03 -0700

    copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int ac, char **av)
 {
-	int finput, foutput, len;
+	int finput, foutput, len = 0;
 	char buff[1024];
 
 	if (ac != 3)
@@ -27,10 +27,9 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 	        exit(99);
 	}
-	while (0 == 0)
+	while ((len < 0)
 	{
-		len = read (finput, buff, sizeof(buff));
-		if (len == -1)
+		if ((len = read (finput, buff, sizeof(buff))) < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
