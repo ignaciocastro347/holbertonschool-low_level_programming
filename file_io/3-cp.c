bd95ea9c428b555ceae14045e2279338732a269c
Ignacio Castro <ignaciocastro347@gmail.com>
2022-07-20 13:10:57 -0700

    copies the content of a file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * main - copy content to a file and paste to another file
+ * @filename: name of file to create
+ * @text_content: content of file to write
+ * Return: 1 on success, -1 on failure
+ */
+int main(int ac, char **av)
+{
+	int finput, foutput, len;
+	char buff[1024];
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	finput = open(av[1], O_RDONLY);
+	if (finput == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+	        exit(98);
+	}
+	foutput = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (foutput == -1)
+	{
+		dprintf(2, "Error: Can't write from file %s\n", av[2]);
+	        exit(99);
+	}
+	while ((len = read (finput, buff, sizeof(buff))) > 0)
+		if (write (foutput, buff, len) != len)
+		{
+			len = -1;
+			break;
+		}
+	if (close(finput) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %i\n", finput);
+		exit(100);
+	}
+	if (close(finput) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %i\n", finput);
+		exit(100);
+	}
+	return (1);
+}
