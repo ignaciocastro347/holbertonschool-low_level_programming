c93553a8c4ba7f542a89aae8a99cb406a69941ee
Ignacio Castro <ignaciocastro347@gmail.com>
2022-08-19 21:40:36 -0300

    Fix Betty errors

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,16 +2,17 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *h = NULL;
-    h = malloc(sizeof(hash_table_t));
-    if (!h)
-        return (NULL);
-    h->size = size;
-    h->array = malloc(size * sizeof(hash_node_t*));
-    if (!h->array)
-    {
-        free(h);
-        return (NULL);
-    }
-    return (h);
+	hash_table_t *h = NULL;
+
+	h = malloc(sizeof(hash_table_t));
+	if (!h)
+		return (NULL);
+	h->size = size;
+	h->array = malloc(size * sizeof(hash_node_t *));
+	if (!h->array)
+	{
+		free(h);
+		return (NULL);
+	}
+	return (h);
 }
