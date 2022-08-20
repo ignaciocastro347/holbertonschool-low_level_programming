139405aa606a9ce4a36f6893271dbb6fb20dc04d
Ignacio Castro <ignaciocastro347@gmail.com>
2022-08-19 23:12:25 -0300

    Create a function that adds an element to the hash table

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add an element to the hash table
+ * @ht: hash table
+ * @key: key value as string
+ * @value: value as string
+ * Return: 1 on succedd or 0 on error
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = key_index((unsigned char *) key, ht->size);
+	hash_node_t *new_hash_node = NULL;
+
+	if (ht->array[index] && strcmp(ht->array[index]->key, key) == 0)
+	{
+		free(ht->array[index]->value);
+		ht->array[index]->value = strdup(value);
+	}
+	else
+	{
+		new_hash_node =  malloc(sizeof(hash_node_t));
+		if (!new_hash_node)
+			return (0);
+
+		new_hash_node->key = strdup(key);
+		new_hash_node->value = strdup(value);
+		new_hash_node->next = ht->array[index] ? ht->array[index] : NULL;
+		ht->array[index] = new_hash_node;
+	}
+	return (1);
+}
